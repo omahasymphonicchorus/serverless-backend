02042a971bc0fc9c458fc1090eaf8c3e24d94f23
Nick Meyer <nick@e3b0c442.io>
2018-08-30 09:14:14 -0500

    recaptcha handling

--- handler.js
+++ handler.js
@@ -1,11 +1,49 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
+const request = require('request-promise-native');
+
+module.exports.contactform = async (event, context) => {
+  
+  // parse the form data
+  const formData = JSON.parse(event.body);
+
+  // build the options for the reCaptcha validation request
+  const opts = {
+    method: 'POST',
+    uri: 'https://www.google.com/recaptcha/api/siteverify',
+    formData: {
+      secret: process.env.RECAPTCHA_SECRET,
+      response: formData['g-recaptcha-response']
+    },
+    json: true
+  }
+  
+  // send the request and handle appropriately
+  let result = await request.post(opts)
+  if(!result.success) {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        error: {
+          code: 403,
+          message: "reCaptcha failed: score " + result.score
+        }
+      })
+    }
+  }
+  
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
+      result: result
     }),
   };
 
