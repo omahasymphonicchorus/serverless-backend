57aa9da51db5b2ab9f43bce25777e29b37e7d6f8
Nick Meyer <nick@e3b0c442.io>
2018-08-30 21:28:27 -0500

    hook up email

--- handler.js
+++ handler.js
@@ -1,51 +1,90 @@
 'use strict';
 
 const request = require('request-promise-native');
+const AWS = require('aws-sdk');
 
-module.exports.contactform = async (event, context) => {
-  
+AWS.config.setPromisesDependency(null);
+const SES = new AWS.SES();
+
+function sendEmail(formData) {
+  const emailParams = {
+    Source: process.env.SRC_ADDRESS,
+    ReplyToAddresses: [formData.email],
+    Destination: {
+      ToAddresses: [process.env.DEST_ADDRESS]
+    },
+    Message: {
+      Body: {
+        Text: {
+          Charset: 'UTF-8',
+          Data: `Name: ${formData.name}
+Email: ${formData.email}
+${formData.phone ? `Phone: ${formData.phone}` : ''}
+Message: ${formData.note}`
+        }
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'OSC contact form submission'
+      }
+    }
+  };
+
+  return SES.sendEmail(emailParams).promise();
+}
+
+module.exports.contactform = async (event, context) => {  
   // parse the form data
   const formData = JSON.parse(event.body);
+  let result = undefined;
 
+  if(context.invokeid != 'id') { // test, do not recaptcha
   // build the options for the reCaptcha validation request
-  const opts = {
-    method: 'POST',
-    uri: 'https://www.google.com/recaptcha/api/siteverify',
-    formData: {
-      secret: process.env.RECAPTCHA_SECRET,
-      response: formData['g-recaptcha-response']
-    },
-    json: true
-  }
-  
-  // send the request and handle appropriately
-  let result = await request.post(opts)
-  if(!result.success) {
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
+    const opts = {
+      method: 'POST',
+      uri: 'https://www.google.com/recaptcha/api/siteverify',
+      formData: {
+        secret: process.env.RECAPTCHA_SECRET,
+        response: formData['g-recaptcha-response']
       },
-      body: JSON.stringify({
-        error: {
-          code: 403,
-          message: "reCaptcha failed: score " + result.score
-        }
-      })
+      json: true
+    }
+    
+    // send the request and handle appropriately
+    result = await request.post(opts)
+    if(!result.success) {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({
+          error: {
+            code: 403,
+            message: "reCaptcha failed: score " + result.score
+          }
+        })
+      }
     }
   }
-  
-  return {
+
+  const ret = {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Origin': '*'
     },
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-      result: result
-    }),
-  };
+    body: ""
+  }
+
+  // send the email
+  result = await sendEmail(formData).then(res => {
+    ret.body = res;
+  }).catch(err => {
+    ret.statusCode = err.statusCode
+    ret.body = JSON.stringify(err);
+  });
+  
+  return ret;
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
